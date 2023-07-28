5d482d2c59e06c5ebc4da158d002e3f56b73c9c0
GitHub Action <action@github.com>
2023-07-28 11:00:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/tschallacka/phpstormxdebugskip/MyAction.kt
+++ src/main/kotlin/com/github/tschallacka/phpstormxdebugskip/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tschallacka.phpstormxdebugskip
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/tschallacka/phpstormxdebugskip/MyBundle.kt
+++ src/main/kotlin/com/github/tschallacka/phpstormxdebugskip/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tschallacka.phpstormxdebugskip
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
